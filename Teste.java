43b91f1ddc6b1de7bed8baca38f210deef25a43e
Vinicius <fritzenvinicius@gmail.com>
2025-08-14 20:42:56 -0300

    first commit

---
+++ Teste.java
@@ -0,0 +1,18 @@
+public class Teste {
+    public static void main(String[] args) {
+        
+        System.out.println("EH JAVA NO TADS com o VINI!!!");
+        System.out.print("TESTE ");
+        System.out.println("SEM LN");
+
+        // comentario para me ajudar
+        System.out.println("OI\n\t1. Amor\n\t2. Baixinho");
+
+        /* PERGUNTAS
+            1. O QUE FAZ O LN? PRINTLN x PRINT
+            2. O QUE FAZ O \n
+            3. O QUE FAZ O \t
+            4. O QUE FAZ O //
+        */
+    }
+}
